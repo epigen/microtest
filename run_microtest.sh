7b2ad8dd07eee4e7a457cf0b82ddbcc167834acd
Andre Rendeiro <afrendeiro@gmail.com>
2016-07-13 16:00:50 +0200

    improve test script

--- run_microtest.sh
+++ run_microtest.sh
@@ -11,16 +11,19 @@
 echo 'Compute node:' `hostname`
 echo 'Start time:' `date +'%Y-%m-%d %T'`
 
-# Setup virtual environment to work
-virtualenv microtestenv
+# Create (isolated) virtual environment
+virtualenv microtestenv --no-site-packages
 
 cd microtestenv
+
+# Make sure there's nothing in your PYTHONPATH
+export PYTHONPATH=
 
 # "Source" to activate the environment
 # this is equivalent to the "source" command
 # but that cannot be executed inside a job/process or
 # would spawn another shell
-. ./bin/activate  
+. ./bin/activate
 
 # install the stack
 pip install https://github.com/epigen/looper/zipball/master
@@ -28,11 +31,13 @@ pip install https://github.com/epigen/pypiper/zipball/master
 
 # clone pipelines
 git clone https://github.com/epigen/open_pipelines
+# install too <- this is just temporary
+pip install https://github.com/epigen/open_pipelines/zipball/master
 
 # clone microtest
 git clone https://github.com/epigen/microtest
 
 # run
-looper -c microtest/config/microtest_config.yaml
+looper --file-checks -c microtest/config/microtest_config.yaml
 
 echo 'End time:' `date +'%Y-%m-%d %T'`
